798d7422cc136b9d014b602e8b94397f742e9e64
AARON <scottaaron@northwesternmutual.com>
2024-05-20 12:51:29 -0500

    add Brandeis to Marquette

--- bostonToMilwaukee.sh
+++ bostonToMilwaukee.sh
@@ -1,3 +1,10 @@
 #! /bin/sh
 
+echo "Boston to Milwaukee: "
 curl -H 'Content-Type: application/json' -X POST -d '{ "firstCoordinate": { "latitude": 43.0389, "longitude": -87.9065 }, "secondCoordinate": { "latitude": 42.3601, "longitude": -71.0589 }, "unit": "miles"}' http://localhost:9080/distance
+
+echo
+echo "Brandeis to Marquette: "
+curl -H 'Content-Type: application/json' -X POST -d '{ "firstCoordinate": { "latitude": 43.039167, "longitude": -87.9325 }, "secondCoordinate": { "latitude": 42.365664, "longitude": -71.259742 }, "unit": "kilometers"}' http://localhost:9080/distance
+
+echo
