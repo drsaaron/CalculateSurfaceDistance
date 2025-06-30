654fc8fa099d57238410737a74182e92036a3e86
AARON <scottaaron@northwesternmutual.com>
2025-06-30 12:10:13 -0500

    version update

--- src/main/java/com/blazartech/products/calculateangulardistance/calc/CalculateAngularDistancePABImpl.java
+++ src/main/java/com/blazartech/products/calculateangulardistance/calc/CalculateAngularDistancePABImpl.java
@@ -9,6 +9,12 @@ import com.blazartech.products.calculateangulardistance.Coordinate;
 import com.blazartech.products.calculateangulardistance.DistanceUnit;
 import static com.blazartech.products.calculateangulardistance.DistanceUnit.kilometers;
 import static com.blazartech.products.calculateangulardistance.DistanceUnit.miles;
+import static java.lang.Math.asin;
+import static java.lang.Math.cos;
+import static java.lang.Math.pow;
+import static java.lang.Math.round;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.toRadians;
 import java.math.BigDecimal;
 import java.util.Map;
 import org.slf4j.Logger;
@@ -23,10 +29,6 @@ import org.springframework.stereotype.Service;
 public class CalculateAngularDistancePABImpl implements CalculateAngularDistancePAB {
 
     private static final Logger logger = LoggerFactory.getLogger(CalculateAngularDistancePABImpl.class);
-    
-    private double toRadians(double degrees) {
-        return Math.toRadians(degrees);
-    }
     
     private static final double EARTH_RADIUS_MILES = 3959; // units are miles, bad scientist
     private static final double EARTH_RADIUS_KILOMETERS = 6378; // km, good scientist
@@ -51,9 +53,9 @@ public class CalculateAngularDistancePABImpl implements CalculateAngularDistance
         double term1 = Math.sin(delLatitude / 2);
         double term2 = Math.sin(delLongitude / 2);
 
-        double delSigma = 2 * Math.asin(Math.sqrt(Math.pow(term1, 2) + Math.cos(firstLatitude) * Math.cos(secondLatitude) * Math.pow(term2, 2)));
+        double delSigma = 2 * asin(sqrt(Math.pow(term1, 2) + cos(firstLatitude) * cos(secondLatitude) * pow(term2, 2)));
         double earthRadius = EARTH_RADIUS.get(distanceUnit);
-        double myDistance = Math.round(earthRadius * delSigma);
+        double myDistance = round(earthRadius * delSigma);
         
         return BigDecimal.valueOf(myDistance);
     }
